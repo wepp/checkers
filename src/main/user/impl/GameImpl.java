6e7ded6828f7487c2a3bfc57c46deedcaff28cc7
Artem <sileage2@gmail.com>
2015-09-24 22:24:42 +0300

    fis ignore

---
+++ src/main/user/impl/GameImpl.java
@@ -0,0 +1,20 @@
+package main.user.impl;
+
+import main.server.vo.Field;
+import main.server.vo.Step;
+import main.user.Game;
+
+/**
+ * Created by Isaiev on 24.09.2015.
+ */
+public class GameImpl implements Game {
+
+    private Field currentField;
+
+    @Override
+    public Step calculateNextStep(Field currentField) {
+        return null;
+    }
+
+
+}
